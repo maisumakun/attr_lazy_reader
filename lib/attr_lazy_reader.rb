4199a16ad811601f5bf7bc23b66ca383dd14eeaa
maisumakun <maisumakun@users.noreply.github.com>
2016-07-27 11:15:03 +0900

    implement code & spec

--- lib/attr_lazy_reader.rb
+++ lib/attr_lazy_reader.rb
@@ -1,5 +1,19 @@
 require "attr_lazy_reader/version"
 
-module AttrLazyReader
-  # Your code goes here...
+# Core extension for Module
+class Module
+  # define lazily initialized read-only attribute.
+  def attr_lazy_reader(sym, &block)
+    raise ArgumentError, 'attr_lazy_reader requires block' unless block
+    raise NameError, "invalid attribute name: #{sym}" unless sym =~ /^[_A-Za-z]\w*$/
+    local_sym = :"@#{sym}"
+    define_method sym do
+      unless instance_variable_defined?(local_sym)
+        instance_variable_set local_sym, instance_eval(&block)
+      end
+      instance_variable_get local_sym
+    end
+  end
+
+  private :attr_lazy_reader
 end
--- spec/attr_lazy_reader_spec.rb
+++ spec/attr_lazy_reader_spec.rb
@@ -4,8 +4,53 @@ describe AttrLazyReader do
   it 'has a version number' do
     expect(AttrLazyReader::VERSION).not_to be nil
   end
+end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+describe Module do
+
+  let(:dummy_class) { Class.new }
+
+  describe '#attr_lazy_reader' do
+    it 'is a private method of Module' do
+      expect(Module.private_instance_methods).to include(:attr_lazy_reader)
+    end
+
+    it 'raises ArgumentError if called without block' do
+      expect { dummy_class.send(:attr_lazy_reader, 'name') }.to raise_error(ArgumentError)
+    end
+
+    it 'raises NameError if called with inappropriate name' do
+      expect { dummy_class.send(:attr_lazy_reader, '1_invalid') {} }.to raise_error(NameError)
+    end
+
+    it 'doesn\'t call block before method call' do
+      expect do |b|
+        dummy_class.send(:attr_lazy_reader, 'foo', &b)
+        instance = dummy_class.new
+      end.not_to yield_control
+    end
+
+    it 'calls block only once' do
+      expect do |b|
+        dummy_class.send(:attr_lazy_reader, 'foo', &b)
+        instance = dummy_class.new
+        instance.foo
+        instance.foo
+      end.to yield_control.once
+    end
+
+    it 'generates method whose return value is from block' do
+      dummy_class.send(:attr_lazy_reader, 'foo') { 5 }
+      instance = dummy_class.new
+      expect(instance.foo).to eq 5
+    end
+
+    it 'evaluates block in instance context' do
+      dummy_class.send(:attr_lazy_reader, 'foo') { @bar }
+      instance = dummy_class.new
+      instance.instance_variable_set :@bar, 80
+      expect(instance.foo).to eq 80
+    end
+
   end
 end
